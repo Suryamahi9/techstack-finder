9ee07a401f44cad012f7c1babc15044774a91551
surya <mahendrasurya2024@gmail.com>
2026-07-29 22:08:31 +0530

    fix: add missing useEffect import in ReverseLookup.js

--- components/ReverseLookup.js
+++ components/ReverseLookup.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 
 const KNOWN_SITES = {
   'React': ['facebook.com', 'github.com', 'netflix.com', 'airbnb.com', 'dropbox.com', 'paypal.com', 'twitter.com', 'figma.com', 'notion.so', 'discord.com'],
